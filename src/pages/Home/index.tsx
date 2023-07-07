abf7574f137fe73a177b46d70e7f6a2351fba13b
PedroHVMagalhaes <magalhaesphvm@gmail.com>
2023-07-06 22:31:46 -0300

    feat: finished step 1

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import Header from '../../components/Header';
+import SenderDataForm from '../../components/SenderDataForm';
 import { Container } from './styles';
 
 const Home = () => {
   return (
     <Container>
       <Header />
+      <SenderDataForm />
     </Container>
   );
 };
